03e91ca8f8b6da71e17d09a073788de69737c538
Osaro Igbinovia <emmyigbinovia@gmail.com>
2025-03-01 23:03:23 +0000

    build: setup SQLAlchemy & PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,14 +1,16 @@
 from app.config import Config
-from app.models.user import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(Config.DATABASE_URL, pool_size=10, max_overflow=20)
+
+engine = create_engine(Config.DATABASE_URL, echo=True, pool_size=10, max_overflow=20)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
 
 def init_db():
     """Initialize database tables."""
 
     from app.models import user 
     Base.metadata.create_all(bind=engine)
-
